64b39c35578e1a5c3058a7c5cfed67139829eb5d
Gabiel679 <gabriel.juan@autonoma.cat>
2021-10-20 15:46:18 +0200

    HOME CREATION

--- php/resource_home.php
+++ php/resource_home.php
@@ -1,1 +1,10 @@
-<?php
+<html lang="ca">
+<head>
+    <title>Home - DISC SHOP</title>
+    <link rel="stylesheet" href="<?php echo BASE_URL . '/estils/categories.css' ?>"/>
+</head>
+<body>
+<header>
+    <?php require __DIR__ . 'controller/nav.php' ?>
+</header>
+</body>
