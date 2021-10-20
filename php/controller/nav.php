89b0893666faf5a0608b71089322a706172b271e
Gabiel679 <gabriel.juan@autonoma.cat>
2021-10-20 15:34:17 +0200

    modificacions navbar

--- php/controller/nav.php
+++ php/controller/nav.php
@@ -1,25 +1,25 @@
 <?php
 
-require_once __DIR__ . 'nav.php/';
+require_once __DIR__ . 'nav.php';
 
-$action = $_GET['action']??null;
+$action = isset($_GET['action']) ? $_GET['action'] : NULL;
 switch ($action){
     case 'home':
-        require __DIR__ . 'nav.php/';
+        require __DIR__ . '../resource_home.php';
         break;
     case 'categories':
-        require __DIR__ . 'nav.php/';
+        require __DIR__ . '../resource_category_list.php';
         break;
     case 'productes':
-        require __DIR__ . 'nav.php/';
+        require __DIR__ . '../resource_product_list.php';
         break;
     case 'register':
-        require __DIR__ . 'nav.php/';
+        require __DIR__ . '../../menu/form-register.html';
         break;
     case 'login':
-        require __DIR__ . 'nav.php/';
+        require __DIR__ . '../../menu/form-login.html';
         break;
     default:
-        require __DIR__ . 'nav.php/'; //Ira al Home
+        require __DIR__ . '../resource_home.php'; //Ira al Home
         break;
 }
--- php/controller/product_list.php
+++ php/controller/product_list.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '../model/connectBD.php';
-require_once __DIR__ . '../products.php.php';
+require_once __DIR__ . '../products.php';
 $products = getProducts();
 require_once __DIR__ . '../view/product_list.php';
