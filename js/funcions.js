108ddbc3f5601b02dfb7d7276a22ff9f0acc55e7
joaquimcodina <joaquim.codina.espin@gmail.com>
2021-11-15 20:34:22 +0100

    Funcions.js, amb JQuery (crides asincrones)

--- js/funcions.js
+++ js/funcions.js
@@ -1,5 +1,40 @@
+function productesCategoria(id){
+    $.get( '/controller/category_list.php', {id : id}, function(resp) {
+        $(".container").html(resp);
+    });
+}
+function productesArtista(id){
+    $.get( '/controller/artists_list.php', {id : id}, function(resp) {
+        $(".container").html(resp);
+    });
+}
+function detallProductes(id){
+    $.get( '/controller/product_list.php', {id : id}, function(resp) {
+        $(".container").html(resp);
+    });
+}
+
+function detallProductesHome1(id){
+    $.get( '/controller/product_list.php', {id : id}, function(resp) {
+        $(".novetats").html(resp);
+        $(".mesVenuts").html("");
+    });
+}
+
+function detallProductesHome2(id){
+    $.get( '/controller/product_list.php', {id : id}, function(resp) {
+        $(".mesVenuts").html(resp);
+        $(".novetats").html("");
+    });
+}
+
 function canviarImatgeDetail(id){
     var ProductImg = document.getElementById("ProductImg");
     var SmallImg = document.getElementsByClassName("small-img");
     ProductImg.src = SmallImg[id].src;
+}
+
+function checkActive(id){
+    var x = document.getElementsByClassName("nav-link");
+    x[id].style.color="springgreen";
 }
