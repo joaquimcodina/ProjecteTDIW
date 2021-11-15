15548fc2111b80febc85ff0e292e486d01edd34b
joaquimcodina <joaquim.codina.espin@gmail.com>
2021-11-15 20:31:27 +0100

    Peu de pagina

---
+++ view/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="footer-content">
+        <h3>Disc Shop</h3>
+        <p>La teva botiga musical de confiança</p>
+        <ul class="menu-footer">
+            <li><a href="<?php echo "?action=home".$action=''?>">Home</a></li>
+            <li><a href="<?php echo "?action=".$action='categories'?>">Categories</a></li>
+            <li><a href="<?php echo "?action=".$action='productes'?>">Productes</a></li>
+            <li><a href="<?php echo "?action=".$action='sobre'?>">Sobre</a></li>
+            <li><a href="<?php echo "?action=".$action='contacte'?>">Contacte</a></li>
+        </ul>
+        <ul class="menu-footer">
+            <li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+        </ul>
+    </div>
+    <div class="footer-bottom">
+        <p>Copyright &copy;. <span>Joaquim Codina i Gabriel Juan</span></p>
+    </div>
+</footer>
