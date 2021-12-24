94321275707b658725f13465581feb95f96aa3f7
joaquimcodina <joaquim.codina.espin@gmail.com>
2021-12-24 10:59:15 +0100

    MVC - Cabàs

--- controller/shopping_cart.php
+++ controller/shopping_cart.php
@@ -1,5 +1,34 @@
 <?php
+    // AFEFIM UNITTAT DEL NOSTRE PRODUCTE AL CABÀS
+    if (isset($_GET['id'])) {
+        include_once __DIR__ ."/../model/connectBD.php";
+        include_once __DIR__ ."/../model/products.php";
+        $prod = $_GET['id'];
+        $connexio = connectaBD();
+        $albums = getProductById($connexio, $prod);
+        foreach($albums as $album){
+            $name = $album['nom_album'];
+            $price = $album['preu'];
+            $img = $album['foto'];
+            $quantity = $_GET['unitsProduct'];
+        }
 
-    include_once __DIR__ . '/../model/connectBD.php';
-    include_once __DIR__ . '/../model/products.php';
-    include_once __DIR__ . '/../model/shopping_cart.php';
+        if (!isset($_SESSION['cart'][$prod])) { //NO TENIM EL PRODUCTE EN EL CABÀS
+            $_SESSION['cart'][$prod] = $prod;
+            $_SESSION['name'][$prod] = $name;
+            $_SESSION['price'][$prod] = $price;
+            $_SESSION['quantity'][$prod] = $quantity;
+            $_SESSION['amount'][$prod] = $price * $quantity;
+            $_SESSION['img'][$prod] = $img;
+
+        } else { //TENIM EL PRODUCTE EN EL CABÀS
+            $_SESSION['quantity'][$prod] = $_SESSION['quantity'][$prod] + $quantity;
+            $_SESSION['amount'][$prod] += $_SESSION['price'][$prod] * $quantity;
+        }
+        $_SESSION['ultimProd'] = $_SESSION['img'][$prod];
+        $_SESSION['totalUnits'] += $quantity;
+        $_SESSION['total'] += $_SESSION['price'][$prod] * $quantity;
+        $alert='Producte afegit correctament!';
+    }
+    include_once __DIR__.'/../view/shopping_cart.php';
+?>
--- view/shopping_cart.php
+++ view/shopping_cart.php
@@ -1,1 +1,18 @@
-<?php
+<div id="container-cart">
+    <table>
+        <?php if(isset($_SESSION['cart'])){
+            foreach ($_SESSION['cart'] as $i) { ?>
+            <tr>
+                <td><img src="<?php echo htmlentities($_SESSION['img'][$i], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?>" width="330px" height="220px" alt="Producte Cabàs"></td>
+                <td><?php echo htmlentities($_SESSION['name'][$i], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?></td>
+                <td><?php echo htmlentities($_SESSION['price'][$i], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?> €</td>
+                <td><?php echo htmlentities($_SESSION['quantity'][$i], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?> Unitat(s)</td>
+                <td><?php echo htmlentities($_SESSION['amount'][$i], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?> €</td>
+                <td><a href="" class="trash-icon"><img src="../img/trash.png" alt="Trash"></a></td>
+            </tr>
+        <?php }} ?>
+    </table>
+    <p>Import Total: <?php echo htmlentities($_SESSION['total'], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?> €</p>
+    <a href="<?php echo htmlentities("?action=".$action='', ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?>" class="btn">Tramitar Comanda</a>
+    <a href="<?php echo "?action=".$action=''?>" class="btn">Esborrar Cabàs</a>
+</div>
