e1f8dd1d4390428645197ff28ff6d97b4fe180d7
joaquimcodina <joaquim.codina.espin@gmail.com>
2021-12-31 15:57:10 +0100

    Guardar totes les dades de l'usuari que ha iniciat sessió en SESSIONS

--- controller/form_login.php
+++ controller/form_login.php
@@ -11,9 +11,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){ //HEM OMPLERT LES DADES DEL FORMULARI
         $login = loginUser($connexio, $_POST['correu']);
         if (password_verify($_POST['password'], $login['password'])) {
             $_SESSION['id_usuari'] =  $login['id_usuari'];
-            $_SESSION['correu'] =  $login['correu'];
             $_SESSION['nom_usuari'] =  $login['nom_usuari'];
+            $_SESSION['correu'] =  $login['correu'];
+            $_SESSION['password'] =  $login['password'];
+            $_SESSION['adreça'] =  $login['adreça'];
+            $_SESSION['poblacio'] =  $login['poblacio'];
+            $_SESSION['codi_postal'] =  $login['codi_postal'];
+            $_SESSION['foto'] =  $login['foto'];
             $alert = 'Benvingut, '.$_SESSION['nom_usuari']."!!";
+            header("Location:index.php?action=''");
 
         } else {
             $alert = 'Usuari i/o contrasenya incorrecte';
