8242126227f5f8eafbdff0ec13309d2ec99c46ad
joaquimcodina <joaquim.codina.espin@gmail.com>
2021-12-31 16:07:55 +0100

    Tancar sessió de l'usuari

--- controller/logout.php
+++ controller/logout.php
@@ -1,8 +1,8 @@
 <?php
-    if(isset($_SESSION["user_id"])){
-        unset($_SESSION["user_id"]);
+    if(isset($_SESSION["id_usuari"])){
+        unset($_SESSION['id_usuari'], $_SESSION['nom_usuari'], $_SESSION['correu'], $_SESSION['password'],
+            $_SESSION['adreça'], $_SESSION['poblacio'], $_SESSION['codi_postal'], $_SESSION['foto']);
     }
-
     header("Location:index.php?action=login");
     exit();
 ?>
---
+++ resource_logout.php
@@ -0,0 +1,22 @@
+<html lang="ca">
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, user-scalable=yes"/>
+        <title>Iniciar Sessió | DISC SHOP</title>
+        <link rel="stylesheet" href="/estils/form.css">
+        <script src="/js/jquery-3.6.0.min.js"></script>
+        <script src="/js/funcions.js"></script>
+        <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+    <?php if(!isset($_GET["id"])){ ?> <!-- Si hi ha ID, no mostris nav, apareix nav extra-->
+        <header>
+            <?php include_once __DIR__.'/controller/nav.php'; ?>
+        </header>
+    <?php }?>
+    <div class="layout">
+        <?php include_once __DIR__.'/controller/logout.php'; ?>
+    </div>
+    </body>
+</html>
